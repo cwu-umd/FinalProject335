652b28479e16fc005178dfccae19f4b576b56eef
Hank Wu <hank@Hanks-MacBook-Air.local>
2022-05-14 00:21:15 -0400

    server initial setup

---
+++ appServer.js
@@ -0,0 +1,22 @@
+let http = require('http');
+let dotenv = require('dotenv');
+let express = require('express');
+
+dotenv.config();
+let app = express();
+app.set('view engine', 'ejs');
+http.createServer(app).listen(1234);
+
+
+
+
+
+
+
+
+// Main page
+app.get('/', (request, response) => {
+    console.log(request.url);
+    response.render("index");
+});
+
