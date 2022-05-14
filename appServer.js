f89a7c675adef30b5b61975967a8709799ad26d0
Hank Wu <hank@Hanks-MacBook-Air.local>
2022-05-14 16:44:16 -0400

    server implemented

--- appServer.js
+++ appServer.js
@@ -12,11 +12,23 @@ http.createServer(app).listen(1234);
 require('dotenv').config();
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
-const db = process.env.MONGO_DB_NAME;
+const db = process.env.MONGO_DB;
 const collection = process.env.MONGO_COLLECTION;
 const {MongoClient, ServerApiVersion} = require('mongodb');
+const { text } = require('body-parser');
 const uri = `mongodb+srv://${userName}:${password}@cluster0.ghxta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
+
+// Amazon Api setup
+const axios = require('axios');
+const { resourceLimits } = require('worker_threads');
+const params = {
+    api_key: process.env.API_KEY,
+    type: "search",
+    amazon_domain: "amazon.com",
+    search_term: ""
+}
+
 
 
 
@@ -41,7 +53,7 @@ app.post('/registrationForm', (request, response) => {
         desc_of_cond: desc_of_cond,
         textbook_name: textbook_name,
     }
-
+    insertTextbook(client, db, collection, variables);
     response.render("processForm", variables);
 })
 
@@ -49,4 +61,112 @@ app.post('/registrationForm', (request, response) => {
 app.get('/search', (request, response) => {
     response.render("search");
 });
+app.post('/search', (request, response) => {
+    let {isbn} = request.body;
+    findTextbook(client, db, collection, isbn).then(res => {
+        // console.log(res);
 
+        let variables = {
+            bookOnShelf: writeToList(res),
+            amazonItem: ""
+        };
+
+        params.search_term = `isbn ${isbn}`;
+
+        axios.get('https://api.rainforestapi.com/request', {params})
+        .then(res => {
+            let item = res.data.search_results[0];
+            let title = item.title;
+            let isbn = item.asin;
+            let price = item.price.raw;
+            let link = item.link;
+            let img = item.image;
+
+            variables.amazonItem = writeToAmazonFormat(title, isbn, price, link, img);
+
+            response.render("processSearch", variables);
+
+        }).catch(error => {
+            console.log(error);
+            console.log("fuck");
+        })
+
+        
+
+    });
+});
+    
+
+
+
+
+
+
+
+
+async function insertTextbook(client, db, collection, record) {
+
+    try {
+        await client.connect();
+
+        let id = await client.db(db)
+                             .collection(collection)
+                             .insertOne(record);
+        
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+
+}
+
+async function findTextbook(client, db, collection, isbn) {
+
+    try {
+        await client.connect();
+        let target = {isbn: isbn};
+
+        const cursor = await client.db(db)
+                             .collection(collection)
+                             .find(target);
+
+        const res = await cursor.toArray();
+        return res;
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }    
+
+}
+
+function writeToList(lstOfBook) {
+    res = "<ol>";
+    lstOfBook.forEach(element => {
+
+        let {name, address, textbook_name, price} = element;
+        let searchRes = searchResult(name, address, textbook_name, price);
+        res += `<li>${searchRes}</li>`;
+        
+    });
+
+    res += "</ol>";
+    return res;
+}
+
+function searchResult(username, email, title, price) {
+
+    return `<strong>Seller's Name: </strong> ${username}<br>
+            <strong>Seller's Email: </strong>${email}<br>
+            <strong>Textbook: </strong>${title}<br>
+            <strong>Price: $</strong>${price}<br>`
+
+}
+
+function writeToAmazonFormat(title, isbn, price, link, img) {
+    return `<img src="${img}" alt="img on Amazon"><br>
+            <strong>Title: </strong> <a href="${link}">${title}</a><br>
+            <strong>ISBN: </strong>${isbn}<br>
+            <strong>Price: </strong>${price}<br><br>`;
+}
